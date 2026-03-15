89bc77ec116972dae40425e5e0da7b0a4731343e
Jonathan Butler <jrbutler@gmail.com>
2026-03-15 19:08:59 -0400

    disable all caching on home page

--- src/app/(app)/home/page.tsx
+++ src/app/(app)/home/page.tsx
@@ -4,6 +4,8 @@ import { redirect } from 'next/navigation'
 import HomeClient from './HomeClient'
 
 export const dynamic = 'force-dynamic'
+export const revalidate = 0
+export const fetchCache = 'force-no-store'
 
 export default async function HomePage() {
   const cookieStore = await cookies()
